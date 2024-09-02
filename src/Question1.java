09fa4defe1b410ee222b5358928d0c06a38d40c6
felipreis <felipreisdaniel@gmail.com>
2024-09-02 15:48:39 -0300

    question 1

--- src/Question1.java
+++ src/Question1.java
@@ -1,18 +1,16 @@
 
-
-
-
-
-public class Question1 {
-
-    public static void main(String[] args) {
-        // Questão 1
+// Questão 1
     /*
         1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
         Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
         Imprimir(SOMA);
         Ao final do processamento, qual será o valor da variável SOMA?
     */
+
+public class Question1 {
+
+    public static void main(String[] args) {
+
 
         Integer indice = 13;
         Integer soma = 0;
